7e2032febcd7ab9eae1984aa2acd68aa367c879b
erasmodominguezdc <erasmo_dominguez@discovery.com>
2021-09-24 08:42:36 +0100

    try first example using the go git client

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/go-git/go-git/v5 v5.4.2
 	github.com/sirupsen/logrus v1.8.1
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
+	gopkg.in/src-d/go-git.v4 v4.13.1 // indirect
 )
--- main.go
+++ main.go
@@ -3,48 +3,43 @@ package gotkube
 /*
 https://chromium.googlesource.com/external/github.com/src-d/go-git/+/8b0c2116cea2bbcc8d0075e762b887200a1898e1/example_test.go
 https://github.com/weaveworks/libgitops
-
+https://pkg.go.dev/github.com/go-git/go-git/v5#example-Clone
  */
 import (
 	"fmt"
+	"gopkg.in/src-d/go-git.v4"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
-	"gopkg.in/src-d/go-git.v4"
-	"gopkg.in/src-d/go-git.v4/config"
-	"gopkg.in/src-d/go-git.v4/plumbing"
-	"gopkg.in/src-d/go-git.v4/plumbing/transport/http"
-	"gopkg.in/src-d/go-git.v4/storage/memory"
-	"gopkg.in/src-d/go-billy.v4/memfs"
 )
 
 
 func main() {
 	fmt.Println("hello world")
-	ExampleClone()
-}
+	// Tempdir to clone the repository
+	dir, err := ioutil.TempDir("", "clone-example")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-func ExampleClone() {
-	// Filesystem abstraction based on memory
-	fs := memfs.New()
-	// Git objects storer based on memory
-	storer := memory.NewStorage()
-	// Clones the repository into the worktree (fs) and storer all the .git
-	// content into the storer
-	_, err := git.Clone(storer, fs, &git.CloneOptions{
+	defer os.RemoveAll(dir) // clean up
+
+	// Clones the repository into the given dir, just as a normal git clone does
+	_, err = git.PlainClone(dir, false, &git.CloneOptions{
 		URL: "https://github.com/git-fixtures/basic.git",
 	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Prints the content of the CHANGELOG file from the cloned repository
-	changelog, err := fs.Open("CHANGELOG")
-	if err != nil {
-		log.Fatal(err)
-	}
-	io.Copy(os.Stdout, changelog)
-	// Output: Initial changelog
-}
 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Prints the content of the CHANGELOG file from the cloned repository
+	changelog, err := os.Open(filepath.Join(dir, "CHANGELOG"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	io.Copy(os.Stdout, changelog)
+}
