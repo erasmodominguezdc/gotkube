c9286c85debb42344cea7518e812c0ad242a81c4
erasmodominguezdc <erasmo_dominguez@discovery.com>
2021-09-25 17:25:24 +0100

    Extracting 2 methods

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ https://chromium.googlesource.com/external/github.com/src-d/go-git/+/8b0c2116cea
 https://github.com/weaveworks/libgitops
 https://pkg.go.dev/github.com/go-git/go-git/v5#example-Clone
  */
+
 import (
-	"fmt"
 	"gopkg.in/src-d/go-git.v4"
 	"io"
 	"io/ioutil"
@@ -15,25 +15,21 @@ import (
 	"path/filepath"
 )
 
-
 func main() {
-	fmt.Println("hello world")
-	// Tempdir to clone the repository
-	dir, err := ioutil.TempDir("", "clone-example")
+
+	dir, err := createRepoDirectory()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	defer os.RemoveAll(dir) // clean up
 
-	// Clones the repository into the given dir, just as a normal git clone does
-	_, err = git.PlainClone(dir, false, &git.CloneOptions{
-		URL: "https://github.com/git-fixtures/basic.git",
-	})
+	os.Setenv("REPO_URL",  "https://github.com/git-fixtures/basic.git")
+	url := os.Getenv("REPO_URL")
 
-	if err != nil {
-		log.Fatal(err)
-	}
+	// Clones the repository into the given dir, just as a normal git clone does
+
+	err = gitClone(err, dir, url)
 
 	// Prints the content of the CHANGELOG file from the cloned repository
 	changelog, err := os.Open(filepath.Join(dir, "CHANGELOG"))
@@ -42,4 +38,24 @@ func main() {
 	}
 
 	io.Copy(os.Stdout, changelog)
+}
+
+func gitClone(err error, dir string, url string) error {
+	_, err = git.PlainClone(dir, false, &git.CloneOptions{
+		URL: url,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return err
+}
+
+func createRepoDirectory() (string, error) {
+	// Tempdir to clone the repository
+	dir, err := ioutil.TempDir("", "clone-example")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return dir, err
 }
