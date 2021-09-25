45b7a369bdfb83e5b7e4b702fd9164d34f58ba61
erasmodominguezdc <erasmo_dominguez@discovery.com>
2021-09-25 17:36:46 +0100

    adding external config

--- go.mod
+++ go.mod
@@ -3,8 +3,14 @@ module gotkube
 go 1.16
 
 require (
-	github.com/go-git/go-git/v5 v5.4.2
-	github.com/sirupsen/logrus v1.8.1
-	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
-	gopkg.in/src-d/go-git.v4 v4.13.1 // indirect
+	github.com/Microsoft/go-winio v0.4.16 // indirect
+	github.com/joho/godotenv v1.4.0
+	github.com/kevinburke/ssh_config v0.0.0-20201106050909-4977a11b4351 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/sergi/go-diff v1.1.0 // indirect
+	github.com/spf13/viper v1.9.0
+	github.com/xanzy/ssh-agent v0.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/src-d/go-git.v4 v4.13.1
 )
--- main.go
+++ main.go
@@ -4,9 +4,12 @@ package main
 https://chromium.googlesource.com/external/github.com/src-d/go-git/+/8b0c2116cea2bbcc8d0075e762b887200a1898e1/example_test.go
 https://github.com/weaveworks/libgitops
 https://pkg.go.dev/github.com/go-git/go-git/v5#example-Clone
+https://towardsdatascience.com/use-environment-variable-in-your-next-golang-project-39e17c3aaa66
+
  */
 
 import (
+	"github.com/spf13/viper"
 	"gopkg.in/src-d/go-git.v4"
 	"io"
 	"io/ioutil"
@@ -58,4 +61,37 @@ func createRepoDirectory() (string, error) {
 		log.Fatal(err)
 	}
 	return dir, err
+}
+
+
+
+// use viper package to read .env file
+// return the value of the key
+func getEnvVariableFromEnvFile(key string) string {
+
+	// SetConfigFile explicitly defines the path, name and extension of the config file.
+	// Viper will use this and not check any of the config paths.
+	// .env - It will search for the .env file in the current directory
+	viper.SetConfigFile(".env")
+
+	// Find and read the config file
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		log.Fatalf("Error while reading config file %s", err)
+	}
+
+	// viper.Get() returns an empty interface{}
+	// to get the underlying type of the key,
+	// we have to do the type assertion, we know the underlying value is string
+	// if we type assert to other type it will throw an error
+	value, ok := viper.Get(key).(string)
+
+	// If the type is a string then ok will be true
+	// ok will make sure the program not break
+	if !ok {
+		log.Fatalf("Invalid type assertion")
+	}
+
+	return value
 }
